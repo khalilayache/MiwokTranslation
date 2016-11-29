4251056e5ed01ccbf30421a7ca49dd9722c526b1
Khalil Ayache <khalilayache@gmail.com>
2016-11-29 19:46:28 -0200

    new activities

---
+++ app/src/main/java/com/khalilayache/miwoktranslation/FamilyActivity.java
@@ -0,0 +1,9 @@
+package com.khalilayache.miwoktranslation;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dlieve_khalil on 29/11/2016.
+ */
+public class FamilyActivity extends AppCompatActivity {
+}
