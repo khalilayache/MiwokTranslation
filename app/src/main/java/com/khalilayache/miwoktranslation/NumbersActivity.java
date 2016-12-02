e452f8147db9565d174b0b2625ad264f82e6f6c2
Khalil Ayache <khalilayache@gmail.com>
2016-12-02 17:23:46 -0200

    Activity Numbers

--- app/src/main/java/com/khalilayache/miwoktranslation/NumbersActivity.java
+++ app/src/main/java/com/khalilayache/miwoktranslation/NumbersActivity.java
@@ -1,9 +1,49 @@
 package com.khalilayache.miwoktranslation;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 /**
- * Created by dlieve_khalil on 29/11/2016.
- */
+ @author by USUARIO
+ @since 01/12/2016
+ @version 1
+ **/
 public class NumbersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> words = new ArrayList<>();
+
+        words.add(getString(R.string.one));
+        words.add(getString(R.string.two));
+        words.add(getString(R.string.three));
+        words.add(getString(R.string.four));
+        words.add(getString(R.string.five));
+        words.add(getString(R.string.six));
+        words.add(getString(R.string.seven));
+        words.add(getString(R.string.eight));
+        words.add(getString(R.string.nine));
+        words.add(getString(R.string.ten));
+
+
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+        for(int i = 0; i < words.size(); i++){
+            TextView textView = new TextView(this);
+            textView.setText(words.get(i));
+            rootView.addView(textView);
+        }
+
+    }
 }
